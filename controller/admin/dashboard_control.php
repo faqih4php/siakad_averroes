7075f99692b0bec204c322053b4f2dfe14a58e60
faqih4php <153882018+faqih4php@users.noreply.github.com>
2024-11-04 16:45:45 +0700

    retrieve data from user in database

---
+++ controller/admin/dashboard_control.php
@@ -0,0 +1,22 @@
+<?php 
+include '../../connection/connection.php';
+
+$sql1 = "SELECT COUNT(*) as jumlah_guru FROM user WHERE role_id = 2";
+$sql2 = "SELECT COUNT(*) as jumlah_siswa FROM user WHERE role_id = 3";
+$sql3 = "SELECT COUNT(*) as jumlah_kelas FROM kelas";
+$sql4 = "SELECT COUNT(*) as jumlah_mapel FROM kelas";
+
+// eksekusi query
+
+$guru = $conn->query($sql1);
+$siswa = $conn->query($sql2);
+$kelas = $conn->query($sql3);
+$mapel = $conn->query($sql4);
+
+// fetch data
+$data_guru = $guru->fetch_assoc();
+$data_siswa = $siswa->fetch_assoc();
+$data_kelas = $kelas->fetch_assoc();
+$data_mapel = $mapel->fetch_assoc();
+
+?>
